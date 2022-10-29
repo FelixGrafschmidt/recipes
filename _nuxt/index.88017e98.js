af39308cf5d9d070a4fe4a612f77c13b64adee36
FelixGrafschmidt <FelixGrafschmidt@users.noreply.github.com>
2022-10-29 16:30:07 +0000

    deploy: 724594d20f67172ec4dcf980f724c7627377d3ab

--- _nuxt/index.88017e98.js
+++
@@ -1,1 +0,0 @@
-import{C as e,o as s,b as t,t as a,u as n,s as r}from"./entry.36864a2a.js";const o={};function c(i,u){return s(),t("div",null,a(n(r)().params.id),1)}const f=e(o,[["render",c]]);export{f as default};
