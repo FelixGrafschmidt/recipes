3407e095ebb44c320699c27d3b69cbdbc6ab28f8
FelixGrafschmidt <drache.dragon@gmail.com>
2023-05-18 13:54:16 +0200

    use vercel/kv instead of local redis

--- server/api/recipes.get.ts
+++ server/api/recipes.get.ts
@@ -1,27 +1,23 @@
-import { createClient } from "redis";
+import { kv } from "@vercel/kv";
 import { Data } from "~~/models/interfaces/Recipe";
 
-const port = parseInt(process.env.REDIS_PORT || "6378");
-const client = createClient({ url: `redis://127.0.0.1:${port}`, database: 2 });
-client.connect();
-
 export default defineEventHandler(async (event) => {
-	if (!event.req.url) {
-		event.res.statusCode = 401;
+	if (!event.node.req.url) {
+		event.node.res.statusCode = 401;
 		return "";
 	}
-	const url = new URL(event.req.url, `http://${event.req.headers.host}`);
+	const url = new URL(event.node.req.url, `http://${event.node.req.headers.host}`);
 	const id = url.searchParams.get("id");
 
 	if (!id) {
-		event.res.statusCode = 401;
+		event.node.res.statusCode = 401;
 		return "";
 	}
 
-	const result: Data = JSON.parse((await client.get(`data-${id}`)) || "{}");
+	const result: Data | null = await kv.get(`data-${id}`);
 
-	if (!result.id) {
-		event.res.statusCode = 404;
+	if (!result?.id) {
+		event.node.res.statusCode = 404;
 		return "";
 	}
 
--- server/api/recipes.post.ts
+++ server/api/recipes.post.ts
@@ -1,25 +1,22 @@
-import { createClient } from "redis";
+import { kv } from "@vercel/kv";
 import { Data } from "~~/models/interfaces/Recipe";
 
-const port = parseInt(process.env.REDIS_PORT || "6378");
-const client = createClient({ url: `redis://127.0.0.1:${port}`, database: 2 });
-client.connect();
-
 export default defineEventHandler(async (event) => {
-	if (!event.req.url) {
-		event.res.statusCode = 401;
+	if (!event.node.req.url) {
+		event.node.req.statusCode = 401;
 		return "";
 	}
-	const url = new URL(event.req.url, `http://${event.req.headers.host}`);
+	const url = new URL(event.node.req.url, `http://${event.node.req.headers.host}`);
 	const id = url.searchParams.get("id");
 
 	if (!id) {
-		event.res.statusCode = 401;
+		event.node.res.statusCode = 401;
 		return "";
 	}
 	const data: Data = await readBody(event);
 
-	await client.set("data-" + data.id, JSON.stringify(data));
-	event.res.statusCode = 204;
+	kv.set("data-" + data.id, data);
+
+	event.node.res.statusCode = 204;
 	return null;
 });
